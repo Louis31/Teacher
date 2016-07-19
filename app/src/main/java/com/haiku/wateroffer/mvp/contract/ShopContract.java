29fa07d8ad108b1176329081cbff4623635e5f74
黄俞铭 <874508605@qq.com>
2016-07-19 23:49:28 +0800

    Finish 修改店铺Logo功能

---
+++ app/src/main/java/com/haiku/wateroffer/mvp/contract/ShopContract.java
@@ -0,0 +1,26 @@
+package com.haiku.wateroffer.mvp.contract;
+
+import com.haiku.wateroffer.mvp.base.BasePresenter;
+import com.haiku.wateroffer.mvp.base.BaseView;
+
+/**
+ * 我的店铺Contract
+ * Created by hyming on 2016/7/19.
+ */
+public interface ShopContract {
+    interface View extends BaseView<Presenter> {
+        // 显示/隐藏加载对话框
+        void showLoadingDialog(boolean isShow);
+
+        // 设置店铺logo
+        void setLogo();
+
+        // 显示信息
+        void showMessage(String msg);
+    }
+
+    interface Presenter extends BasePresenter {
+        // 修改店铺logo
+        void changeShopLogo(int uid, String data);
+    }
+}
---
+++ app/src/main/java/com/haiku/wateroffer/mvp/persenter/ShopPresenter.java
@@ -0,0 +1,62 @@
+package com.haiku.wateroffer.mvp.persenter;
+
+import android.support.annotation.NonNull;
+
+import com.haiku.wateroffer.common.util.net.IRequestCallback;
+import com.haiku.wateroffer.mvp.contract.ShopContract;
+import com.haiku.wateroffer.mvp.model.IUserModel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 我的店铺Presenter
+ * Created by hyming on 2016/7/19.
+ */
+public class ShopPresenter implements ShopContract.Presenter, IRequestCallback {
+
+    @NonNull
+    private final IUserModel mUserModel;
+    @NonNull
+    private final ShopContract.View mView;
+
+    public ShopPresenter(@NonNull IUserModel userModel, @NonNull ShopContract.View view) {
+        this.mUserModel = userModel;
+        this.mView = view;
+        mView.setPresenter(this);
+    }
+
+    /**
+     * Presenter 接口方法
+     */
+
+    @Override
+    public void changeShopLogo(int uid, String data) {
+        mView.showLoadingDialog(true);
+        Map<String, Object> params = new HashMap<>();
+        params.put("uid", uid);
+        params.put("data", data);
+        mUserModel.changeShopLogo(params, this);
+    }
+
+    /**
+     * Callback 接口方法
+     */
+    @Override
+    public void getTokenSuccess(Map<String, Object> params) {
+        mUserModel.changeShopLogo(params, this);
+    }
+
+    // 成功回调
+    @Override
+    public void onSuccess() {
+        mView.showLoadingDialog(false);
+        mView.setLogo();
+    }
+
+    @Override
+    public void onError(int errorCode, String errorMsg) {
+        mView.showLoadingDialog(false);
+        mView.showMessage(errorMsg);
+    }
+}
