2ff5a3b11efbad5c8fd875f69cd884f52b0d5de8
黄俞铭 <874508605@qq.com>
2016-07-20 11:49:55 +0800

    Add DeliverContract.xml

---
+++ app/src/main/java/com/haiku/wateroffer/mvp/contract/DeliverContract.java
@@ -0,0 +1,36 @@
+package com.haiku.wateroffer.mvp.contract;
+
+import com.haiku.wateroffer.bean.Deliver;
+import com.haiku.wateroffer.bean.OrderItem;
+import com.haiku.wateroffer.mvp.base.BasePresenter;
+import com.haiku.wateroffer.mvp.base.BaseView;
+
+import java.util.List;
+
+/**
+ * 配送列表的Contract
+ * Created by hyming on 2016/7/20.
+ */
+public interface DeliverContract {
+    interface View extends BaseView<Presenter> {
+        // 显示/隐藏加载对话框
+        void showLoadingDialog(boolean isShow);
+
+        // 显示列表界面
+        void showListView(List<Deliver> list);
+
+        // 更新列表界面
+        void updateListView(int status);
+
+        // 显示信息
+        void showMessage(String msg);
+    }
+
+    interface Presenter extends BasePresenter {
+        // 获取列表数据
+        void getDeliverList(int uid);
+
+        // 修改配送员状态
+        void changeDeliverStatus(int diliveryman_id, String phone, int status);
+    }
+}
