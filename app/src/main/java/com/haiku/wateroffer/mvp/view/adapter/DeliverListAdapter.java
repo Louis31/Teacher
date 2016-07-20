df10bed1a25b9cce111dbc823cb5530a31e91dba
黄俞铭 <874508605@qq.com>
2016-07-20 11:06:26 +0800

    Change 完成DeliverListAdapter.java逻辑

--- app/src/main/java/com/haiku/wateroffer/mvp/view/adapter/DeliverListAdapter.java
+++ app/src/main/java/com/haiku/wateroffer/mvp/view/adapter/DeliverListAdapter.java
@@ -2,14 +2,13 @@ package com.haiku.wateroffer.mvp.view.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView.ViewHolder;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.haiku.wateroffer.R;
 import com.haiku.wateroffer.bean.Deliver;
-import com.haiku.wateroffer.common.listener.MyItemClickListener;
+import com.haiku.wateroffer.constant.TypeConstant;
 
 import java.util.List;
 
@@ -18,12 +17,26 @@ import java.util.List;
  * Created by hyming on 2016/7/20.
  */
 public class DeliverListAdapter extends MyBaseAdapter {
+    public final static int TYPE_LIST = 1;
+    public final static int TYPE_EDIT = 2;
+
+    private int mType;
+    private String pauseStr;
+    private String continueStr;
 
     private List<Deliver> mDatas;
+    private DeliverListListener mListener;
 
-    public DeliverListAdapter(Context cxt, List<Deliver> datas) {
+    public void setListener(DeliverListListener listener) {
+        this.mListener = listener;
+    }
+
+    public DeliverListAdapter(Context cxt, List<Deliver> datas, int type) {
         super(cxt);
         this.mDatas = datas;
+        this.mType = type;
+        pauseStr = cxt.getString(R.string.pause);
+        continueStr = cxt.getString(R.string.go_on);
     }
 
     @Override
@@ -33,11 +46,34 @@ public class DeliverListAdapter extends MyBaseAdapter {
 
         vh.tv_deliver_name.setText(bean.getDiliveryman_name());
         vh.tv_deliver_phone.setText(bean.getDiliveryman_phone());
+
+        // 编辑列表
+        if (mType == TYPE_EDIT) {
+            if (TypeConstant.Deliver.PAUSE == bean.getDiliveryman_status()) {
+                // 暂停状态，可以点击继续
+                vh.tv_continue_pause.setText(continueStr);
+            } else if (TypeConstant.Deliver.CONTINUE == bean.getDiliveryman_status()) {
+                // 继续状态，可以点击暂停
+                vh.tv_continue_pause.setText(pauseStr);
+            }
+        }
+        // 配送列表
+        else if (mType == TYPE_LIST) {
+            if (TypeConstant.Deliver.PAUSE == bean.getDiliveryman_status()) {
+                // 暂停状态
+                vh.tv_paused.setVisibility(View.VISIBLE);
+                vh.tv_added.setVisibility(View.GONE);
+            } else if (TypeConstant.Deliver.CONTINUE == bean.getDiliveryman_status()) {
+                // 继续状态
+                vh.tv_added.setVisibility(View.VISIBLE);
+                vh.tv_paused.setVisibility(View.GONE);
+            }
+        }
     }
 
     @Override
     protected ViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
-        return new ItemViewHolder(mInflater.inflate(R.layout.item_deliver_list, parent, false));
+        return new ItemViewHolder(mInflater.inflate(R.layout.item_deliver_list, parent, false), mListener);
     }
 
     @Override
@@ -49,11 +85,54 @@ public class DeliverListAdapter extends MyBaseAdapter {
     class ItemViewHolder extends ViewHolder {
         TextView tv_deliver_name;
         TextView tv_deliver_phone;
+        TextView tv_added;
+        TextView tv_paused;
+        View llayout_operation;
+        TextView tv_continue_pause;
+        TextView tv_delete;
 
-        public ItemViewHolder(View v) {
+        public ItemViewHolder(View v, DeliverListListener listener) {
             super(v);
             tv_deliver_name = bind(v, R.id.tv_deliver_name);
             tv_deliver_phone = bind(v, R.id.tv_deliver_phone);
+            tv_added = bind(v, R.id.tv_added);
+            tv_paused = bind(v, R.id.tv_paused);
+            llayout_operation = bind(v, R.id.llayout_operation);
+            tv_continue_pause = bind(v, R.id.tv_continue_pause);
+            tv_delete = bind(v, R.id.tv_delete);
+
+            // 配送员列表
+            if (mType == TYPE_LIST) {
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (listener != null) {
+                            listener.onItemClick(getPosition());
+                        }
+                    }
+                });
+            }
+            // 编辑列表
+            else if (mType == TYPE_EDIT) {
+                llayout_operation.setVisibility(View.VISIBLE);
+                tv_continue_pause.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (listener != null) {
+                            listener.onContinuePauseClick(getPosition());
+                        }
+                    }
+                });
+
+                tv_delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (listener != null) {
+                            listener.onDeleteClick(getPosition());
+                        }
+                    }
+                });
+            }
         }
     }
 }
