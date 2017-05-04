73091f96a8baad894334402320bb381975a6b09a
LouisZ <80824619@qq.com>
2017-05-04 16:17:40 +0800

    base

--- app/src/main/java/com/haiku/wateroffer/constant/ActionConstant.java
+++
@@ -1,9 +0,0 @@
-package com.haiku.wateroffer.constant;
-
-/**
- * Created by hyming on 2016/7/22.
- */
-public class ActionConstant {
-    public final static String REFRESH_ORDER_LIST = "com.haiku.wateroffer.action.refresh_order_list";
-    public final static String REFRESH_GOODS_LIST = "com.haiku.wateroffer.action.refresh_goods_list";
-}
--- app/src/main/java/com/haiku/wateroffer/mvp/base/BasePresenter.java
+++
@@ -1,8 +0,0 @@
-package com.haiku.wateroffer.mvp.base;
-
-/**
- * Presenter基类接口
- * Created by hyming on 2016/7/5.
- */
-public interface BasePresenter {
-}
